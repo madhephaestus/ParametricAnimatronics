9ca46314d11f9715b60ca88524696fd049fcdd24
MyFuturePC <MyFuturePC@DESKTOP-8PMUUUQ>
2016-06-16 19:08:59 -0400

    adding servos

--- AnimatronicHead.groovy
+++ AnimatronicHead.groovy
@@ -1,5 +1,7 @@
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
+
+
 ArrayList<CSG> makeHead(){
 	//Set up som parameters to use
 	LengthParameter thickness 		= new LengthParameter("Material Thickness",3.5,[10,1])
@@ -7,6 +9,27 @@ ArrayList<CSG> makeHead(){
 	LengthParameter snoutLen 		= new LengthParameter("Snout Length",headDiameter.getMM(),[200,50])
 	LengthParameter jawHeight 		= new LengthParameter("Jaw Height",50,[200,10])
 	LengthParameter JawSideWidth 		= new LengthParameter("Jaw Side Width",20,[40,10])
+	double jawAttachOffset =  (headDiameter.getMM()/2
+				-thickness.getMM()/2 
+				-thickness.getMM())
+				
+	CSG jawServo = (CSG)ScriptingEngine
+	                    .gitScriptRun(
+                                "https://gist.github.com/3f9fef17b23acfadf3f7.git", // git location of the library
+	                              "servo.groovy" , // file to load
+	                              ["hv6214mg"]
+                        )
+                        .roty(90)
+                        .rotz(90)
+                        .movez(jawHeight.getMM())
+                        
+	CSG smallServo = (CSG)ScriptingEngine
+	                    .gitScriptRun(
+                                "https://gist.github.com/3f9fef17b23acfadf3f7.git", // git location of the library
+	                              "servo.groovy" , // file to load
+	                              ["towerProMG91"]
+                        )
+                        
 
 	CSG mechPlate =new Cylinder(	headDiameter.getMM()/2,
 							headDiameter.getMM()/2,
@@ -32,20 +55,32 @@ ArrayList<CSG> makeHead(){
 	
 	mechPlate=mechPlate 
 		.movez(jawHeight.getMM())
-
+	
+	
 	CSG sideJaw = new Cube(
 			JawSideWidth.getMM(),
 			thickness.getMM(),
 			jawHeight.getMM()+thickness.getMM()
 			).toCSG()
 			.movez(jawHeight.getMM()/2 +thickness.getMM()/2 )
-	CSG LeftSideJaw =sideJaw
-			.movey(
-				headDiameter.getMM()/2
-			) 
+			.union(new Cylinder(	JawSideWidth.getMM()/2,
+								JawSideWidth.getMM()/2,
+								thickness.getMM(),(int)30).toCSG()
+					.movez(-thickness.getMM()/2)
+					.rotx(90)
+					.movez(jawHeight.getMM() +thickness.getMM() )
+							)
 	
+				
+	CSG LeftSideJaw =sideJaw
+			.movey(jawAttachOffset) 
+	CSG RightSideJaw =sideJaw
+			.movey(-jawAttachOffset) 
+
+	mechPlate = mechPlate.difference(LeftSideJaw.scalex(1.5),RightSideJaw.scalex(1.5))
+	bottomJaw = bottomJaw.difference(LeftSideJaw,RightSideJaw)
 		
-	def returnValues = 	[mechPlate,bottomJaw,LeftSideJaw]
+	def returnValues = 	[mechPlate,bottomJaw,LeftSideJaw,RightSideJaw,jawServo,smallServo]
 	for (int i=0;i<returnValues.size();i++){
 		int index = i
 		returnValues[i] = returnValues[i]
